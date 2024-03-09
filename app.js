b53c12fb0754be23e7e7f2f183fab1238f86e68c
prakashjaiswal <prakashjaiswal625@gmail.com>
2024-03-09 10:50:54 +0530

    server readme file on base url

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
 const s3_routes = require('./src/routes/s3-routes');
-app.use(express.json()); // Enable parsing of JSON request bodies
-app.use(express.urlencoded({ extended: true })); // Enable parsing of URL-encoded request bodies
-app.use('/', s3_routes); // Mount the routes defined in routes.js at the root URL path
+// Enable parsing of JSON request bodies
+app.use(express.json()); 
+// Enable parsing of URL-encoded request bodies
+app.use(express.urlencoded({ extended: true })); 
+// Mount the routes defined in routes.js at the root URL path
+app.use('/', s3_routes); 
+
+const fs = require('fs');
+const path = require('path');
+
+
+// Define a route to serve the README file
+app.get('/', (req, res) => {
+    const readmePath = path.join(__dirname, 'README.md');
+
+    // Read the README file
+    fs.readFile(readmePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading README file:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        // Send the content of the README file as HTML
+        res.send(`<pre>${data}</pre>`);
+    });
+});
 
 app.listen(3000, () => console.log('Server started on port 3000')); // Start the server listening on port 3000
